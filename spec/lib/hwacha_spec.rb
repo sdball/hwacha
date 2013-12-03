a14958f76c29b449e0cf728203a1b19ae09f479c
Stephen Ball <sdball@gmail.com>
2013-12-03 00:48:30 -0500

    Add VCR gem for much more reasonable specs

--- spec/lib/hwacha_spec.rb
+++ spec/lib/hwacha_spec.rb
@@ -1,22 +1,34 @@
 require_relative '../../lib/hwacha'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/cassettes'
+  c.hook_into :typhoeus
+end
 
 describe Hwacha do
   let(:page_with_success_response) { 'rakeroutes.com' }
 
   describe "#check" do
     it "yields when there is a successful web response" do
-      expect { |probe| subject.check(page_with_success_response, &probe) }.to yield_control
+      VCR.use_cassette('page_with_success_response') do
+        expect { |probe| subject.check(page_with_success_response, &probe) }.to yield_control
+      end
     end
 
     it "yields the checked URL" do
-      subject.check(page_with_success_response) do |url, response|
-        expect(url).to eq "HTTP://%s/" % page_with_success_response
+      VCR.use_cassette('page_with_success_response') do
+        subject.check(page_with_success_response) do |url, response|
+          expect(url).to eq "HTTP://%s/" % page_with_success_response
+        end
       end
     end
 
     it "yields the web response" do
-      subject.check(page_with_success_response) do |url, response|
-        expect(response.success?).to be_true
+      VCR.use_cassette('page_with_success_response') do
+        subject.check(page_with_success_response) do |url, response|
+          expect(response.success?).to be_true
+        end
       end
     end
   end
