53174ccbad00c7a8e687c8d3ae3586f3dd49d08c
Stephen Ball <sdball@gmail.com>
2013-12-03 00:52:40 -0500

    Spec more types of possible responses

--- spec/lib/hwacha_spec.rb
+++ spec/lib/hwacha_spec.rb
@@ -8,11 +8,25 @@ end
 
 describe Hwacha do
   let(:page_with_success_response) { 'rakeroutes.com' }
+  let(:page_with_404_response) { 'rakeroutes.com/this-page-does-not-exist' }
+  let(:not_a_page) { '' }
 
   describe "#check" do
     it "yields when there is a successful web response" do
       VCR.use_cassette('page_with_success_response') do
         expect { |probe| subject.check(page_with_success_response, &probe) }.to yield_control
+      end
+    end
+
+    it "yields when there is not a successful web response" do
+      VCR.use_cassette('page_with_404_response') do
+        expect { |probe| subject.check(page_with_404_response, &probe) }.to yield_control
+      end
+    end
+
+    it "yields when there is no web response" do
+      VCR.use_cassette('not_a_page') do
+        expect { |probe| subject.check(not_a_page, &probe) }.to yield_control
       end
     end
 
