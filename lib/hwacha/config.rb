e150476e9f617427de390d1090b42c97aa272e0c
Stephen Ball <sdball@gmail.com>
2014-01-12 09:28:03 -0500

    Add creation of options hash to Hwacha::Config
    Instead of putting data fields into Hwacha::Config and then pulling them
    out by name, this new options method will allow us to have the
    configuration object be responsible for giving us back the set options
    as a configuration for Typhoeus.

--- lib/hwacha/config.rb
+++ lib/hwacha/config.rb
@@ -2,5 +2,16 @@ require 'ostruct'
 
 class Hwacha
   class Config < OpenStruct
+    def options
+      options = {}
+      options.merge(concurrency_option)
+    end
+
+    private
+
+    def concurrency_option
+      return {} if max_concurrent_requests.nil?
+      { :max_concurrency => max_concurrent_requests }
+    end
   end
 end
